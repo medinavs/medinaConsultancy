98f93b4a78a79e765585c387b3d91ca1e78864ac
murilo <murilorockstar492@gmail.com>
2025-01-24 13:59:28 -0300

    fix: update API proxy handler to use function and adjust rewrite destination

--- src/api/proxy.js
+++ src/api/proxy.js
@@ -1,9 +1,12 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-export default createProxyMiddleware({
-  target: 'https://uj58p9e8la.execute-api.us-east-2.amazonaws.com',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': '/dev/consultancy/search',
-  },
-});
+export default function handler(req, res) {
+  const proxy = createProxyMiddleware({
+    target: 'https://uj58p9e8la.execute-api.us-east-2.amazonaws.com',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api': '/dev/consultancy/search',
+    },
+  });
+  return proxy(req, res);
+}
