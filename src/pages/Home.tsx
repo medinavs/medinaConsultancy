f6d2b8caa795120331283ca08f673abd0ebdf256
murilo <murilorockstar492@gmail.com>
2025-01-24 13:36:24 -0300

    fix: update image source path for medina logo in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import medinaLogo from '../assets/medinaLogo.jpg'
 import { motion } from 'framer-motion'
 import { Button } from '../components/ui/button'
 import { ArrowCircleRight } from '@phosphor-icons/react'
@@ -22,7 +21,7 @@ export function Home() {
         <div className="h-screen w-screen bg-slate-950 overflow-hidden flex items-center justify-center">
             <motion.div key="modal" exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 2 } }}>
                 <main className="flex flex-col items-center justify-center gap-8 pb-20">
-                    <img src={medinaLogo} alt="Medina Logo" className="h-32 w-32 rounded-lg" />
+                    <img src={"/src/assets/medinaLogo.jpg"} alt="Medina Logo" className="h-32 w-32 rounded-lg" />
                     <div className="flex flex-col items-center gap-4">
                         <h2 className="text-3xl text-slate-200 font-bold">Bem-vindo ao MedinaConsultancy</h2>
                         <p className="text-slate-200 text-center opacity-11">O seu parceiro de negócios para a transformação digital</p>
